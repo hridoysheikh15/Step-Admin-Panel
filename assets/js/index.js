41867bd0f33ced3de1539a0f86e4c855d4734017
hridoysheikh15 <ridoysheikh15@gmail.com>
2025-10-21 20:16:08 +0600

    Final output

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,21 @@
+// Side bar set
+$(document).ready(function () {
+    function checkWidth() {
+        if ($(window).width() < 1024) {
+            $('body').addClass('side-hide');
+        } else {
+            $('body').removeClass('side-hide');
+        }
+    }
+
+    checkWidth();
+
+    $(window).resize(function () {
+        checkWidth();
+    });
+
+});
+
 // sidebar function
 function sidebar() {
     $("body").toggleClass("side-hide");
